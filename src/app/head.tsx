8a27c0de41a4c931e66b5f196b9c4d1c46d8d66d
fullstackDEVH <huagiahuy20052001@gmail.com>
2023-03-07 14:59:10 +0700

    feat: building folder

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+      <>
+        <title>IMDB - CLONE</title>
+        <meta name="description" content="This is a IMDB website" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </>
+    );
+  }

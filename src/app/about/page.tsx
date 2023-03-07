d5e53d2795c8ca2a26f28571ad74d8e627f60876
fullstackDEVH <huagiahuy20052001@gmail.com>
2023-03-07 17:05:06 +0700

    feat : create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,27 @@
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-3">
+      <h2 className="text-2xl text-amber-500 font-medium">about</h2>
+      <p>
+        IMDb (an abbreviation of Internet Movie Database)[2] is an online
+        database of information related to films, television series, podcasts,
+        home videos, video games, and streaming content online – including cast,
+        production crew and personal biographies, plot summaries, trivia,
+        ratings, and fan and critical reviews.
+      </p>
+      <p>
+        IMDb began as a fan-operated movie database on the Usenet group
+        rec.arts.movies in 1990, and moved to the Web in 1993. Since 1998, it is
+        now owned and operated by IMDb.com, Inc., a subsidiary of Amazon.
+      </p>
+      <p>
+        As of March 2022, the database contained some 10.1 million titles
+        (including television episodes) and 11.5 million person records.[3]
+        Additionally, the site had 83 million registered users. The site`s
+        message boards were disabled in February 2017.
+      </p>
+    </div>
+  );
+};
+
+export default About;

4bf4d04515236a07fd15629336fe4affff0b3b98
fullstackDEVH <huagiahuy20052001@gmail.com>
2023-03-07 23:44:00 +0700

    main dev

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
       <>
-        <title>IMDB - CLONE 123</title>
+        <title>IMDB - CLONE - main - dev</title>
         <meta name="description" content="This is a IMDB website" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </>

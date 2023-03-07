c6e2a4c8fce8f8e03912fa4debfa9e948820e2fe
fullstackDEVH <huagiahuy20052001@gmail.com>
2023-03-07 20:47:49 +0700

    feat : interactive api TMDB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
 
-export default function Home() {
+const fetchTrending = async (genre : string) => {
+  const key = process.env.API_TMDB_KEY;
+
+  const res= await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${key}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  return await res.json();
+}
+
+export default async function Home( { searchParams } : any ) {
+  // console.log(process.env.API_TMDB);
+  const genreType = searchParams.genre ?? "fetchTrending";
+  const data = await fetchTrending(genreType);
+  
   return (
     <div className="text-red-600">Home</div>
   )

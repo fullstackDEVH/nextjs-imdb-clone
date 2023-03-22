ca7fc22c4f27d3424c4f746a0770a31f718c5e95
fullstackDEVH <huagiahuy20052001@gmail.com>
2023-03-23 00:55:11 +0700

    tsst

--- src/app/Test.tsx
+++ src/app/Test.tsx
@@ -4,28 +4,27 @@ import { useEffect, useState } from "react";
 
 const Test = () => {
     const [test, setTest ] = useState({name : "huy"});
-  useEffect(() => {
+ 
     const data = async () => {
-      const res = await fetch("http://113.160.226.174:50003/api/auth/login", {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify({ username: "huy", password: "huy20052001" }),
-      });
+        const res = await fetch("http://113.160.226.174:50003/api/auth/login", {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          credentials: "include",
+          body: JSON.stringify({ username: "huy", password: "huy20052001" }),
+        });
+  
+        const data = await res.json();
+  
+        setTest(data)
+  
+      };
 
-      const data = await res.json();
-
-      setTest(data)
-
-    };
-
-    data();
-  }, []);
   return <div>test
     <p>
+        <button onClick={data}>lick heaerre</button>
         {
             JSON.stringify(test)
         }

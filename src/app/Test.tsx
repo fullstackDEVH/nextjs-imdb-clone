3fd746c69724e69cc6f37e3a3f1a4b9e548c2567
fullstackDEVH <huagiahuy20052001@gmail.com>
2023-03-23 00:59:28 +0700

    ewq

--- src/app/Test.tsx
+++ src/app/Test.tsx
@@ -5,31 +5,11 @@ import { useEffect, useState } from "react";
 const Test = () => {
   const [test, setTest] = useState({ name: "huy" });
 
-  const data = async () => {
-    try {
-      const res = await fetch("http://113.160.226.174:50003/api/auth/login", {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify({ username: "huy", password: "huy20052001" }),
-      });
-
-      const data = await res.json();
-
-      setTest(data);
-    } catch (error) {
-        alert(error)
-    }
-  };
 
   return (
     <div>
       test
       <p>
-        <button onClick={data}>lick heaerre</button>
         {JSON.stringify(test)}
       </p>
     </div>
